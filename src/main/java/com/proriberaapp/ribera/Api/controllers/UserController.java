a6a5e45c47fc319dd18fef6442ac9ea505ed77ca
webjose87 <webjose87@gmail.com>
2024-03-04 16:13:47 -0500

    Registro

--- src/main/java/com/proriberaapp/ribera/Api/controllers/UserController.java
+++ src/main/java/com/proriberaapp/ribera/Api/controllers/UserController.java
@@ -1,4 +1,25 @@
 package com.proriberaapp.ribera.Api.controllers;
 
+import com.proriberaapp.ribera.Domain.entities.UserEntity;
+import com.proriberaapp.ribera.Infraestructure.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody UserEntity user) {
+        userRepository.insert(user);
+    }
 }
