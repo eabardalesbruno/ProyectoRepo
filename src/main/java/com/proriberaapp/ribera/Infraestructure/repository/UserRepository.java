b181c2ca11cc64c1009d88e0902497f3b9052816
webjose87 <webjose87@gmail.com>
2024-03-05 11:34:43 -0500

    Registro

--- src/main/java/com/proriberaapp/ribera/Infraestructure/repository/UserRepository.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/repository/UserRepository.java
@@ -1,26 +1,12 @@
 package com.proriberaapp.ribera.Infraestructure.repository;
 
 import com.proriberaapp.ribera.Domain.entities.UserEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-
 @Repository
-public class UserRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public UserRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
-    public void insert(UserEntity user) {
-        String sql = "INSERT INTO user (firstname, lastname, email) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, user.getFirstName(), user.getLastName(), user.getEmail());
-    }
-
-    public void save(UserEntity userEntity) {
-        insert(userEntity);
-    }
+public interface UserRepository extends ReactiveCrudRepository<UserEntity, Integer>, R2dbcRepository<UserEntity, Integer> {
 }
+
+
--- src/main/java/com/proriberaapp/ribera/Infraestructure/services/UserService.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/services/UserService.java
@@ -1,4 +1,34 @@
 package com.proriberaapp.ribera.Infraestructure.services;
 
+import com.proriberaapp.ribera.Domain.entities.UserEntity;
+import com.proriberaapp.ribera.Infraestructure.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Flux<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Mono<UserEntity> getUserById(Integer userId) {
+        return userRepository.findById(userId);
+    }
+
+    public Mono<UserEntity> createUser(UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    public Mono<Void> deleteUser(Integer userId) {
+        return userRepository.deleteById(userId);
+    }
+
+    public Mono<UserEntity> saveUser(UserEntity user) {
+        return userRepository.save(user);
+    }
 }
