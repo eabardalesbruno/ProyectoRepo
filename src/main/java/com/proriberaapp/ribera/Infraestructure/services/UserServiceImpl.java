2b6c438d5cabc2bb7d779009c0106b312a230822
bruno <bruno_1416@hotmail.com>
2024-03-08 10:38:52 -0500

    sin transacionalidad

--- src/main/java/com/proriberaapp/ribera/Infraestructure/services/UserServiceImpl.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/services/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.proriberaapp.ribera.Crosscutting.security.JwtTokenProvider;
 import com.proriberaapp.ribera.Domain.entities.UserEntity;
 import com.proriberaapp.ribera.Infraestructure.repository.UserRepository;
 import com.proriberaapp.ribera.Infraestructure.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,19 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private JwtTokenProvider jwtUtil;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtUtil;
 
     @Override
-    @Transactional
     public Mono<UserEntity> registerUser(UserEntity user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
