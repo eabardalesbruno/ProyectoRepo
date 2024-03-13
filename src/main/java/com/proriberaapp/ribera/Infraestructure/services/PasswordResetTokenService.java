6ec122b53b39b32b204a8003408d0e1d91983cb0
webjose87 <webjose87@gmail.com>
2024-03-12 19:21:48 -0500

    Password Restablecimiento

--- src/main/java/com/proriberaapp/ribera/Infraestructure/services/PasswordResetTokenService.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/services/PasswordResetTokenService.java
@@ -5,9 +5,9 @@ import com.proriberaapp.ribera.Domain.entities.UserEntity;
 import com.proriberaapp.ribera.Infraestructure.repository.PasswordResetTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Random;
 
@@ -21,20 +21,22 @@ public class PasswordResetTokenService {
         this.tokenRepository = tokenRepository;
     }
 
-    public PasswordResetTokenEntity generateToken(UserEntity user) {
+    public Mono<PasswordResetTokenEntity> generateToken(UserEntity user) {
         String token = generateRandomToken();
         PasswordResetTokenEntity resetToken = new PasswordResetTokenEntity();
-        resetToken.setUserid(Long.valueOf(user.getUserId()));
+        resetToken.setUserid(user.getUserId().longValue());
         resetToken.setToken(token);
         resetToken.setExpiryDate(Timestamp.valueOf(LocalDateTime.now().plusMinutes(3))); // Token válido por 3 minutos
-        return tokenRepository.save(resetToken);
+        return Mono.just(tokenRepository.save(resetToken));
     }
 
     public boolean verifyToken(UserEntity user, String token) {
         PasswordResetTokenEntity resetToken = tokenRepository.findByUserAndToken(user.getUserId(), token);
         return resetToken != null && resetToken.getExpiryDate().after(Timestamp.valueOf(LocalDateTime.now()));
     }
-
+    public PasswordResetTokenEntity saveToken(PasswordResetTokenEntity tokenEntity) {
+        return tokenRepository.save(tokenEntity);
+    }
     private String generateRandomToken() {
         Random random = new Random();
         int token = 100000 + random.nextInt(900000);
