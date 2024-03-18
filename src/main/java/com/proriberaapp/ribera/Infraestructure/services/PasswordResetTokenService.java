e06b995092c032721c9c8053e066841d8a2d69ad
webjose87 <webjose87@gmail.com>
2024-03-18 16:46:12 -0500

    Password Restablecimiento Validacion y Confirmacion

--- src/main/java/com/proriberaapp/ribera/Infraestructure/services/PasswordResetTokenService.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/services/PasswordResetTokenService.java
@@ -1,15 +1,10 @@
 package com.proriberaapp.ribera.Infraestructure.services;
 
 import com.proriberaapp.ribera.Domain.entities.PasswordResetTokenEntity;
-import com.proriberaapp.ribera.Domain.entities.UserClientEntity;
 import com.proriberaapp.ribera.Infraestructure.repository.PasswordResetTokenRepository;
-import com.proriberaapp.ribera.Infraestructure.repository.UserClientRepository;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -19,13 +14,10 @@ import java.util.Random;
 @Service
 public class PasswordResetTokenService {
     private final PasswordResetTokenRepository tokenRepository;
-    private static final Logger logger = LoggerFactory.getLogger(PasswordResetTokenService.class);
-    private final PostgresqlConnectionFactory connectionFactory;
 
     @Autowired
-    public PasswordResetTokenService(PasswordResetTokenRepository tokenRepository, PostgresqlConnectionFactory connectionFactory) {
+    public PasswordResetTokenService(PasswordResetTokenRepository tokenRepository) {
         this.tokenRepository = tokenRepository;
-        this.connectionFactory = connectionFactory;
     }
 
     public Mono<PasswordResetTokenEntity> generateToken(Integer userId, String token, Timestamp expiryDate) {
@@ -40,7 +32,6 @@ public class PasswordResetTokenService {
     public Mono<PasswordResetTokenEntity> generateTokenAndSave(Integer userId) {
         String token = generateRandomToken();
         Timestamp expiryDate = Timestamp.valueOf(LocalDateTime.now().plusMinutes(3));
-        executeInsertQuery(userId, token, expiryDate);
         PasswordResetTokenEntity resetToken = PasswordResetTokenEntity.builder()
                 .userClientId(userId)
                 .token(token)
@@ -48,35 +39,7 @@ public class PasswordResetTokenService {
                 .expiryDate(expiryDate)
                 .build();
 
-        logger.info("Consulta de inserción: INSERT INTO passwordresettoken (userid, token, passwordstate, expirydate) " +
-                "VALUES (" + userId + ", '" + token + "', 0, '" + expiryDate + "')");
-
         return tokenRepository.save(resetToken);
-    }
-
-    private void executeInsertQuery(Integer userId, String token, Timestamp expiryDate) {
-        String insertQuery = "INSERT INTO passwordresettoken (userclientid, token, passwordstate, expirydate) VALUES ("
-                + userId + ", '" + token + "', 0, '" + expiryDate + "')";
-
-        connectionFactory.create()
-                .flatMapMany(connection -> connection.createStatement(insertQuery)
-                        .execute()
-                        .flatMap(result -> result.map((row, rowMetadata) -> row.get(0, Integer.class)))
-                )
-                .flatMap(rowsUpdated -> {
-                    if (rowsUpdated == 1) {
-                        return Mono.empty();
-                    } else {
-                        return Mono.error(new RuntimeException("Error al insertar en la tabla passwordresettoken"));
-                    }
-                })
-                .subscribe(
-                        success -> logger.info("Inserción en la tabla passwordresettoken exitosa"),
-                        error -> {
-                            logger.error("Error al insertar en la tabla passwordresettoken: " + error.getMessage());
-                            error.printStackTrace();
-                        }
-                );
     }
 
     public void markTokenAsUsed(Integer userId) {
@@ -99,5 +62,4 @@ public class PasswordResetTokenService {
         int token = 100000 + random.nextInt(900000);
         return String.valueOf(token);
     }
-
 }
