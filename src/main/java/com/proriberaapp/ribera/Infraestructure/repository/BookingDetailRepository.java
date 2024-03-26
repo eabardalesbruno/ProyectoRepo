aa5b3c4c091528581e40568f26b6e3dead3ca77a
webjose87 <webjose87@gmail.com>
2024-03-26 13:47:16 -0500

    Payment Book APIs

--- src/main/java/com/proriberaapp/ribera/Infraestructure/repository/BookingDetailRepository.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/repository/BookingDetailRepository.java
@@ -5,20 +5,15 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 public interface BookingDetailRepository extends R2dbcRepository<BookingDetailEntity, Integer> {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     Mono<BookingDetailEntity> findByBookingId(BookingDetailEntity bookingDetailEntity);
     Flux<BookingDetailEntity> findAllByBookingIdIn(List<BookingDetailEntity> bookingDetailEntity);
-=======
-=======
->>>>>>> jose-dev
+
     Mono<BookingDetailEntity> findByRoomIdAndBookingIdAndPaymentStateId(Integer roomId, Integer bookingId, Integer paymentStateId);
     Flux<BookingDetailEntity> findByRoomIdAndBookingIdAndPaymentStateId(Flux<Integer> roomId, Flux<Integer> bookingId, Flux<Integer> paymentStateId);
     Flux<BookingDetailEntity> findByRoomType(String roomType);
 
-<<<<<<< HEAD
->>>>>>> jose-dev
-=======
->>>>>>> jose-dev
 }
