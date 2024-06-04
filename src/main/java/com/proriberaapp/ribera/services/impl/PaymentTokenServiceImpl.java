a13fc3c5a0a72c77d8fcc88549c8a859a7b8700a
jmanrique5 <consultorweb_csti@centria.net>
2024-06-04 00:22:13 -0500

    Al validar token devuelve el pago

--- src/main/java/com/proriberaapp/ribera/services/impl/PaymentTokenServiceImpl.java
+++ src/main/java/com/proriberaapp/ribera/services/impl/PaymentTokenServiceImpl.java
@@ -1,10 +1,7 @@
 package com.proriberaapp.ribera.services.impl;
 
-import com.proriberaapp.ribera.Domain.entities.BookingEntity;
-import com.proriberaapp.ribera.Domain.entities.PaymentBookEntity;
-import com.proriberaapp.ribera.Domain.entities.PaymentTokenEntity;
-import com.proriberaapp.ribera.Infraestructure.repository.PaymentBookRepository;
-import com.proriberaapp.ribera.Infraestructure.repository.PaymentTokenRepository;
+import com.proriberaapp.ribera.Domain.entities.*;
+import com.proriberaapp.ribera.Infraestructure.repository.*;
 import com.proriberaapp.ribera.services.PaymentBookService;
 import com.proriberaapp.ribera.services.PaymentTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +20,33 @@ public class PaymentTokenServiceImpl implements PaymentTokenService {
     private final PaymentTokenRepository paymentTokenRepository;
     private final PaymentBookService paymentBookService;
     private final PaymentBookRepository paymentBookRepository;
-
+    private final BookingRepository bookingRepository;
+    private final UserClientRepository userClientRepository;
+    private final PaymentMethodRepository paymentMethodRepository;
+    private final PaymentStateRepository paymentStateRepository;
+    private final PaymentTypeRepository paymentTypeRepository;
+    private final CurrencyTypeRepository currencyTypeRepository;
 
     @Autowired
-    public PaymentTokenServiceImpl(PaymentTokenRepository paymentTokenRepository, PaymentBookService paymentBookService,PaymentBookRepository paymentBookRepository) {
+    public PaymentTokenServiceImpl(
+            PaymentTokenRepository paymentTokenRepository,
+            PaymentBookService paymentBookService,
+            PaymentBookRepository paymentBookRepository,
+            BookingRepository bookingRepository,
+            UserClientRepository userClientRepository,
+            PaymentMethodRepository paymentMethodRepository,
+            PaymentStateRepository paymentStateRepository,
+            PaymentTypeRepository paymentTypeRepository,
+            CurrencyTypeRepository currencyTypeRepository) {
         this.paymentTokenRepository = paymentTokenRepository;
         this.paymentBookService = paymentBookService;
         this.paymentBookRepository = paymentBookRepository;
+        this.bookingRepository = bookingRepository;
+        this.userClientRepository = userClientRepository;
+        this.paymentMethodRepository = paymentMethodRepository;
+        this.paymentStateRepository = paymentStateRepository;
+        this.paymentTypeRepository = paymentTypeRepository;
+        this.currencyTypeRepository = currencyTypeRepository;
     }
 
     @Override
@@ -84,15 +101,31 @@ public class PaymentTokenServiceImpl implements PaymentTokenService {
                     String linkPayment = "https://ribera-dev.inclub.world/payment-validation?token=" + paymentToken;
                     if (now.before(paymentTokenEntity.getEndDate())) {
                         return paymentBookService.getPaymentBookById(paymentTokenEntity.getPaymentBookId())
-                                .map(paymentBook -> {
-                                    Map<String, Object> response = new HashMap<>();
-                                    response.put("active", true);
-                                    response.put("paymentBook", paymentBook);
-                                    response.put("token", paymentToken);
-                                    response.put("linkPayment", linkPayment);
-                                    response.put("status", 1);
-                                    response.put("message", "Token validado");
-                                    return response;
+                                .flatMap(paymentBook -> {
+                                    Mono<BookingEntity> bookingMono = bookingRepository.findById(paymentBook.getBookingId());
+                                    Mono<UserClientEntity> userClientMono = userClientRepository.findById(paymentBook.getUserClientId());
+                                    Mono<PaymentMethodEntity> paymentMethodMono = paymentMethodRepository.findById(paymentBook.getPaymentMethodId());
+                                    Mono<PaymentStateEntity> paymentStateMono = paymentStateRepository.findById(paymentBook.getPaymentStateId());
+                                    Mono<PaymentTypeEntity> paymentTypeMono = paymentTypeRepository.findById(paymentBook.getPaymentTypeId());
+                                    Mono<CurrencyTypeEntity> currencyTypeMono = currencyTypeRepository.findById(paymentBook.getCurrencyTypeId());
+
+                                    return Mono.zip(bookingMono, userClientMono, paymentMethodMono, paymentStateMono, paymentTypeMono, currencyTypeMono)
+                                            .map(tuple -> {
+                                                Map<String, Object> response = new HashMap<>();
+                                                response.put("active", true);
+                                                response.put("paymentBook", paymentBook);
+                                                response.put("booking", tuple.getT1());
+                                                response.put("userClient", tuple.getT2());
+                                                response.put("paymentMethod", tuple.getT3());
+                                                response.put("paymentState", tuple.getT4());
+                                                response.put("paymentType", tuple.getT5());
+                                                response.put("paymentSubType", tuple.getT6());
+                                                response.put("token", paymentToken);
+                                                response.put("linkPayment", linkPayment);
+                                                response.put("status", 1);
+                                                response.put("message", "Token validado");
+                                                return response;
+                                            });
                                 });
                     } else {
                         Map<String, Object> response = new HashMap<>();
