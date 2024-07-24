2409b560f9289a3547068306b8067f5743c882cb
bruno <bruno_1416@hotmail.com>
2024-07-24 09:10:57 -0500

    cambios

--- src/main/java/com/proriberaapp/ribera/Api/controllers/payme/entity/PayMeAuthorization.java
+++ src/main/java/com/proriberaapp/ribera/Api/controllers/payme/entity/PayMeAuthorization.java
@@ -9,8 +9,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-import java.util.Map;
-import java.util.List;
 
 @Getter
 @Setter
