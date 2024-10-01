459f7f4825483cd5729c25ba6fba461bcf639e83
Moises Molina <w7moises@gmail.com>
2024-10-01 13:11:49 -0500

    add: room detail service

--- src/main/java/com/proriberaapp/ribera/Api/controllers/admin/ManagerRoomDetailController.java
+++ src/main/java/com/proriberaapp/ribera/Api/controllers/admin/ManagerRoomDetailController.java
@@ -3,10 +3,8 @@ package com.proriberaapp.ribera.Api.controllers.admin;
 import com.proriberaapp.ribera.Domain.entities.RoomDetailEntity;
 import com.proriberaapp.ribera.services.client.RoomDetailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -15,8 +13,29 @@ import reactor.core.publisher.Mono;
 public class ManagerRoomDetailController extends BaseManagerController<RoomDetailEntity, RoomDetailEntity> {
     private final RoomDetailService roomDetailService;
 
+    @GetMapping
+    public Flux<RoomDetailEntity> findAll() {
+        return roomDetailService.findAll();
+    }
+
     @GetMapping("/{id}")
     public Mono<RoomDetailEntity> findById(@PathVariable Integer id) {
         return roomDetailService.findById(id);
+    }
+
+    @PostMapping
+    public Mono<RoomDetailEntity> create(@RequestBody RoomDetailEntity roomDetailEntity) {
+        return roomDetailService.save(roomDetailEntity);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<RoomDetailEntity> update(@PathVariable Integer id, @RequestBody RoomDetailEntity roomDetailEntity) {
+        roomDetailEntity.setRoomDetailId(id);
+        return roomDetailService.update(roomDetailEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable Integer id) {
+        return roomDetailService.deleteById(id);
     }
 }
