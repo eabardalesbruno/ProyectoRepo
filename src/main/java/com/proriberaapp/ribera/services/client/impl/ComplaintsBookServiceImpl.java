53e6c90bd217642ab98d828f0dcbe977ff28f916
Moises Molina <w7moises@gmail.com>
2024-10-07 13:36:53 -0500

    fix: change email

--- src/main/java/com/proriberaapp/ribera/services/client/impl/ComplaintsBookServiceImpl.java
+++ src/main/java/com/proriberaapp/ribera/services/client/impl/ComplaintsBookServiceImpl.java
@@ -32,7 +32,7 @@ public class ComplaintsBookServiceImpl implements ComplaintsBookService {
                 .flatMap(savedComplaint -> {
                     String subject = "Nuevo Reclamo Recibido";
                     String body = generateEmailBody(savedComplaint);
-                    String primaryRecipient = "notificacionesinclub@inclub.site";
+                    String primaryRecipient = "reclamosriberadelrio@inresorts.club";
                     String userEmail = savedComplaint.getEmail();
 
                     // Enviar el correo al destinatario principal y una copia al email del usuario
