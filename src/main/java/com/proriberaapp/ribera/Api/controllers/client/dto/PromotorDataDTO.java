031e44804a026261a8afacf4a6211a5005975a3a
Kevin Rojas Caceres <82854850+kvnrc2012@users.noreply.github.com>
2024-11-04 08:09:42 -0500

    AkRojas - Update Tasks

---
+++ src/main/java/com/proriberaapp/ribera/Api/controllers/client/dto/PromotorDataDTO.java
@@ -0,0 +1,35 @@
+package com.proriberaapp.ribera.Api.controllers.client.dto;
+
+import com.proriberaapp.ribera.Domain.entities.UserClientEntity;
+import com.proriberaapp.ribera.Domain.entities.UserPromoterEntity;
+import lombok.Data;
+import reactor.core.publisher.Mono;
+
+import java.sql.Timestamp;
+
+@Data
+public class PromotorDataDTO {
+    private Integer userPromotorId;
+    private String username;
+    private String documentNumber;
+    private String email;
+    private String phone;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+
+    public Mono<PromotorDataDTO> convertTo(UserPromoterEntity promotor) {
+         PromotorDataDTO promotorDTO = new PromotorDataDTO();
+        promotorDTO.setUserPromotorId(promotor.getUserPromoterId());
+        promotorDTO.setPhone(promotor.getPhone());
+        promotorDTO.setUsername(promotor.getUsername());
+        promotorDTO.setFirstName(promotor.getFirstName());
+        promotorDTO.setLastName(promotor.getLastName());
+        promotorDTO.setDocumentNumber(promotor.getDocumentNumber());
+        promotorDTO.setEmail(promotor.getEmail());
+        promotorDTO.setAddress(promotor.getAddress());
+
+        return Mono.just(promotorDTO);
+    }
+}
