4473884cce8cc64371099c47edf776f97f285064
Thelinch1452 <tonygreys2008@gmail.com>
2024-11-20 17:28:19 -0500

    Agregar pruebas para el envío y almacenamiento de facturas en VisualContIntegrationTest

--- src/test/java/com/proriberaapp/ribera/Infraestructure/invoice/VisualContIntegrationTest.java
+++ src/test/java/com/proriberaapp/ribera/Infraestructure/invoice/VisualContIntegrationTest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,7 +24,14 @@ import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 import org.springframework.web.reactive.function.client.WebClient.RequestBodyUriSpec;
 import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
 import org.springframework.web.reactive.function.client.WebClient.RequestHeadersUriSpec;
+
+import com.proriberaapp.ribera.Domain.entities.CurrencyTypeEntity;
+import com.proriberaapp.ribera.Domain.entities.Invoice.InvoiceEntity;
+import com.proriberaapp.ribera.Domain.entities.Invoice.InvoiceItemEntity;
+import com.proriberaapp.ribera.Domain.entities.Invoice.InvoiceStatusEntity;
+import com.proriberaapp.ribera.Domain.entities.Invoice.InvoiceTypeEntity;
 import com.proriberaapp.ribera.Domain.enums.invoice.InvoiceCurrency;
+import com.proriberaapp.ribera.Domain.enums.invoice.InvoiceStatus;
 import com.proriberaapp.ribera.Domain.enums.invoice.InvoiceType;
 import com.proriberaapp.ribera.Domain.invoice.CompanyDomain;
 import com.proriberaapp.ribera.Domain.invoice.InvoiceClientDomain;
@@ -32,6 +40,11 @@ import com.proriberaapp.ribera.Domain.invoice.InvoiceItemDomain;
 import com.proriberaapp.ribera.Domain.invoice.InvoiceResponse;
 import com.proriberaapp.ribera.Domain.invoice.InvoiceResponseStatus;
 import com.proriberaapp.ribera.Domain.invoice.SunatInvoice;
+import com.proriberaapp.ribera.Infraestructure.repository.CurrencyTypeRepository;
+import com.proriberaapp.ribera.Infraestructure.repository.Invoice.InvoiceItemRepository;
+import com.proriberaapp.ribera.Infraestructure.repository.Invoice.InvoiceRepository;
+import com.proriberaapp.ribera.Infraestructure.repository.Invoice.InvoiceStateRepository;
+import com.proriberaapp.ribera.Infraestructure.repository.Invoice.InvoiceTypeRepsitory;
 
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -47,6 +60,20 @@ public class VisualContIntegrationTest {
 
         @Autowired
         private SunatInvoice sunatInvoice;
+        @Autowired
+        private InvoiceStateRepository invoiceStatusRepository;
+
+        @Autowired
+        private InvoiceTypeRepsitory invoiceTypeRepsitory;
+
+        @Autowired
+        private InvoiceRepository invoiceRepsitory;
+
+        @Autowired
+        private CurrencyTypeRepository currencyTypeRepository;
+
+        @Autowired
+        private InvoiceItemRepository invoiceItemRepository;
 
         @BeforeEach
         public void setUp() {
@@ -187,4 +214,62 @@ public class VisualContIntegrationTest {
                                 })
                                 .verifyComplete();
         }
+
+        @Test
+        public void savedAndSenInvoice() {
+                CompanyDomain company = new CompanyDomain("ddd", "1233", "wdwd", "dwdwd", "wdwdw", "wdwdw", "wdwd");
+                InvoiceClientDomain client = new InvoiceClientDomain("Juan Perez", "78804372", "Av. Los Pinos",
+                                "123456789",
+                                "");
+                InvoiceDomain invoiceDomain = new InvoiceDomain(client, 1, 18.0, 1, InvoiceCurrency.PEN);
+                invoiceDomain.addItem(new InvoiceItemDomain("Item 1", "aaa", 1, new BigDecimal(50)));
+                invoiceDomain.addItem(new InvoiceItemDomain("Item 2", "aaa", 1, new BigDecimal(20)));
+                invoiceDomain.addItem(new InvoiceItemDomain("Item 3", "aaa", 1, new BigDecimal(30)));
+                invoiceDomain.calculatedTotals();
+                List<InvoiceItemEntity> items = invoiceDomain.getItems()
+                                .stream()
+                                .map(item -> item.toEntity(invoiceDomain.getId()))
+                                .collect(Collectors.toList());
+                Mono<InvoiceTypeEntity> invoiceTypeEntity = this.invoiceTypeRepsitory
+                                .findByName(invoiceDomain.getType())
+                                .switchIfEmpty(Mono.error(new IllegalArgumentException("Invalid type")));
+                Mono<CurrencyTypeEntity> currencyTypeEntity = this.currencyTypeRepository
+                                .findByCurrencyTypeName(invoiceDomain.getCurrency().getCurrency())
+                                .switchIfEmpty(Mono.error(new IllegalArgumentException("Invalid currency")));
+                Mono<InvoiceResponse> response = sunatInvoice.sendInvoice(invoiceDomain, company);
+                Mono<InvoiceEntity> data = response.flatMap(responseInvoice -> {
+                        invoiceDomain.setKeySupplier(responseInvoice.getKey());
+                        invoiceDomain.setSupplierNote(responseInvoice.getSunat_note());
+                        invoiceDomain.setStatus(responseInvoice.isAceptada_por_sunat() ? InvoiceStatus.ACEPTED
+                                        : InvoiceStatus.REJECTED);
+                        return Mono.just(invoiceDomain);
+                }).flatMap(invoiceD -> {
+                        return Mono.zip(invoiceTypeEntity, this.invoiceStatusRepository
+                                        .findByName(invoiceDomain.getStatus().getStatus())
+                                        .switchIfEmpty(Mono.error(new IllegalArgumentException(
+                                                        "Invalid status"))),
+                                        currencyTypeEntity).flatMap(tuple -> {
+                                                InvoiceTypeEntity invoiceType = tuple.getT1();
+                                                invoiceDomain.setCorrelative(invoiceType.getCorrelative());
+                                                InvoiceStatusEntity invoiceStatus = tuple.getT2();
+                                                CurrencyTypeEntity currencyType = tuple.getT3();
+                                                InvoiceEntity entity = invoiceDomain.toEntity(invoiceType.getId(),
+                                                                invoiceStatus.getId(), 1,
+                                                                currencyType.getCurrencyTypeId());
+                                                return this.invoiceRepsitory.save(entity)
+                                                                .flatMap(savedEntity -> this.invoiceItemRepository
+                                                                                .saveAll(items)
+                                                                                .then(this.invoiceTypeRepsitory
+                                                                                                .addCorrelative(invoiceDomain
+                                                                                                                .getType()))
+                                                                                .then(Mono.just(savedEntity)));
+                                        });
+                });
+
+                System.out.println("LLEGO ANTES DE VERIFICAR");
+                StepVerifier.create(data)
+                                .expectNextCount(1)
+                                .verifyComplete();
+        }
+
 }
