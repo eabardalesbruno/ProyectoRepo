55139b1617e89ad92581b66d2574c0ce3068276a
Thelinch1452 <tonygreys2008@gmail.com>
2024-12-02 16:32:05 -0500

    feat: eliminar método findById en UserClientRepository

--- src/main/java/com/proriberaapp/ribera/Infraestructure/repository/UserClientRepository.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/repository/UserClientRepository.java
@@ -21,8 +21,6 @@ public interface UserClientRepository extends R2dbcRepository<UserClientEntity, 
 
     Flux<UserClientEntity> findAll();
 
-    Mono<UserClientEntity> findById(Integer id);
-
     Mono<Void> deleteById(Integer id);
 
     Mono<UserClientEntity> findByUserClientId(Integer userClientId);
