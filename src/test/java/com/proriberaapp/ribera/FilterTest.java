5839b42600c5e9430b31920144d4d125e8c7750c
Thelinch1452 <tonygreys2008@gmail.com>
2024-12-23 17:43:08 -0500

    Añadir nuevos campos de capacidad y estado de reserva en las entidades de habitación y ajustar consultas relacionadas

--- src/test/java/com/proriberaapp/ribera/FilterTest.java
+++ src/test/java/com/proriberaapp/ribera/FilterTest.java
@@ -1,9 +1,20 @@
 package com.proriberaapp.ribera;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.proriberaapp.ribera.Api.controllers.admin.dto.views.ViewRoomOfferReturn;
+import com.proriberaapp.ribera.services.client.RoomOfferService;
+
+import net.bytebuddy.asm.MemberSubstitution.Substitution.Chain.Step;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 public class FilterTest {
+    @Autowired
+    private RoomOfferService roomOfferService;
 
     void filtroPorFechas() {
 
@@ -15,5 +26,17 @@ public class FilterTest {
 
     void filtroPorNumeroTotalPersonas() {
 
+    }
+
+    @Test
+    void filtrarDepartamentosConMaximaCapacidad() {
+        String categoryName = "DEPARTAMENTO";
+        /*
+         * Flux<ViewRoomOfferReturn> view =
+         * roomOfferService.findFilteredV2(categoryName, null, null, 0, 3, 2, 2, 0,
+         * null,
+         * false);
+         * StepVerifier.create(view).expectNextCount(7).verifyComplete();
+         */
     }
 }
