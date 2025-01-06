685b7bcd5a6b53b32ba1e9b2f022c353c7635d08
ErickLG281 <elozanogomez281@gmail.com>
2025-01-06 16:18:37 -0500

    Add commission method

---
+++ src/main/java/com/proriberaapp/ribera/Domain/entities/CommissionEntity.java
@@ -0,0 +1,48 @@
+package com.proriberaapp.ribera.Domain.entities;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("commission")
+public class CommissionEntity {
+    @Id
+    @Column("commissionid")
+    private Integer commissionId;
+
+    @Column("paymentbookid")
+    private Integer paymentBookId;
+
+    @Column("promoterid")
+    private Integer promoterId;
+
+    @Column("partnerid")
+    private Integer partnerId;
+
+    @Column("commissionamount")
+    private BigDecimal commissionAmount;
+
+    @Column("riberaamount")
+    private BigDecimal riberaAmount;
+
+    @Column("partnerpayment")
+    private BigDecimal partnerPayment;
+
+    @Column("adminfee")
+    private BigDecimal adminFee;
+
+    @Column("servicefee")
+    private BigDecimal serviceFee;
+
+    @Column("casetype")
+    private Integer caseType;
+
+}
