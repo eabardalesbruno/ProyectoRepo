9884b8b34df54191ee340d307073e424f20b9b46
Thelinch1452 <tonygreys2008@gmail.com>
2025-01-08 10:25:36 -0500

    add BookingStateDto and update BookingService and BookingController to handle booking state updates

---
+++ src/main/java/com/proriberaapp/ribera/Api/controllers/client/dto/BookingStateDto.java
@@ -0,0 +1,12 @@
+package com.proriberaapp.ribera.Api.controllers.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BookingStateDto {
+    private Integer bookingStateId;
+    private String bookingStateName;
+
+}
