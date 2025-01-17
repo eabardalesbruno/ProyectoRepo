dfcc24157f7c7c3326ba414aa957dea2cdd34d89
Maycol Yauri <yauri_95@hotmail.com>
2025-01-17 09:38:14 -0500

    avance dashboard

--- src/main/java/com/proriberaapp/ribera/services/client/impl/GenerateReportServiceImpl.java
+++ src/main/java/com/proriberaapp/ribera/services/client/impl/GenerateReportServiceImpl.java
@@ -9,6 +9,7 @@ import com.proriberaapp.ribera.Infraestructure.repository.DocumentTypeRepository
 import com.proriberaapp.ribera.Infraestructure.repository.RoomRepository;
 import com.proriberaapp.ribera.services.PDFGeneratorService;
 import com.proriberaapp.ribera.services.client.GenerateReportService;
+import static com.proriberaapp.ribera.services.PDFGeneratorService.generateReservationPdfFromHtml;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FileUtils;
 import org.jose4j.base64url.internal.apache.commons.codec.binary.Base64;
@@ -36,26 +37,34 @@ public class GenerateReportServiceImpl implements GenerateReportService {
 
     private final RoomRepository roomRepository;
 
-    private final PDFGeneratorService pdfGeneratorService;
-
     @Override
     public Mono<ResponseEntity<ResponseFileDto>> generateReportReservation(int idReservation) {
-        return bookingRepository.findByBookingId(idReservation).publishOn(Schedulers.boundedElastic()).flatMap(bookingEntity -> {
+        return bookingRepository.findByBookingId(idReservation).flatMap(bookingEntity -> {
             ReservationReportDto reservationDto = new ReservationReportDto();
             reservationDto.setPdfFileName("reserva.pdf");
             reservationDto.setDateReservation(bookingEntity.getCreatedAt().toLocalDateTime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+            reservationDto.setDayBookingInit(bookingEntity.getDayBookingInit().toLocalDateTime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+            reservationDto.setDayBookingEnd(bookingEntity.getDayBookingEnd().toLocalDateTime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+            reservationDto.setNumberAdults((bookingEntity.getNumberAdults()+bookingEntity.getNumberAdultsMayor()+bookingEntity.getNumberAdultsExtra())+" Adultos");
+            reservationDto.setNumberChildren(bookingEntity.getNumberChildren()+" Niños");
+            reservationDto.setNumberBabies(bookingEntity.getNumberBabies()+" Bebés");
             List<ReservationReportDto> listCompanions = new ArrayList<>();
+            return bookingRepository.fingMethodPäymentByBookingId(idReservation).publishOn(Schedulers.boundedElastic()).flatMap(methodPayment -> {
+            reservationDto.setMethodPayment(methodPayment);
 
             List<CompanionsDto> items = companionsRepository.getCompanionsByBookingId(bookingEntity.getBookingId()).collectList().block();
 
             return roomRepository.getRoomNameByBookingId(idReservation).flatMap(roomEntity -> {
                 reservationDto.setRoomName(roomEntity.getRoomName());
+                reservationDto.setRoomNumber(roomEntity.getRoomNumber());
                 for (CompanionsDto item : items) {
                     if (item.isTitular()) {
                         reservationDto.setDocumentType(item.getDocumenttypedesc());
                         reservationDto.setDocumentNumber(item.getDocumentNumber());
                         reservationDto.setFullname(item.getFirstname() + " " + item.getLastname());
                         reservationDto.setYears(item.getYears() != null ? String.valueOf(item.getYears()) : "");
+                        reservationDto.setEmail(item.getEmail() != null ? item.getEmail() : "");
+                        reservationDto.setCountrydesc(item.getCountrydesc() != null ? item.getCountrydesc() : "");
                     } else {
                         ReservationReportDto reportDto = new ReservationReportDto();
                         reportDto.setDocumentType(item.getDocumenttypedesc() != null ? item.getDocumenttypedesc() : "" );
@@ -70,7 +79,7 @@ public class GenerateReportServiceImpl implements GenerateReportService {
                 byte[] encoded = null;
                 ResponseFileDto result = new ResponseFileDto();
                 try {
-                    File pdfFile = pdfGeneratorService.generateReservationPdfFromHtml(reservationDto);
+                    File pdfFile = generateReservationPdfFromHtml(reservationDto);
                     encoded = Base64.encodeBase64(FileUtils.readFileToByteArray(pdfFile));
                     result.setFile(new String(encoded, StandardCharsets.US_ASCII));
                     result.setFilename(pdfFile.getName());
@@ -79,6 +88,7 @@ public class GenerateReportServiceImpl implements GenerateReportService {
                     return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result));
                 }
                 return Mono.just(ResponseEntity.ok(result));
+            });
             });
         });
     }
