c43dade20d3d63648da0df6a44ac20fbcbbf8d0f
ErickLG281 <elozanogomez281@gmail.com>
2025-02-26 16:35:13 -0500

    Addition of functional days for tickefullday

--- src/main/java/com/proriberaapp/ribera/Infraestructure/repository/TicketEntryFullDayRepository.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/repository/TicketEntryFullDayRepository.java
@@ -1,10 +1,19 @@
 package com.proriberaapp.ribera.Infraestructure.repository;
 
 import com.proriberaapp.ribera.Domain.entities.TicketEntryFullDayEntity;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface TicketEntryFullDayRepository extends R2dbcRepository<TicketEntryFullDayEntity, Integer> {
+
+    @Query("UPDATE ticketentryfullday SET enableddays = :enabledDays")
+    Mono<Void> actualizarEnabledDaysParaTodos(String enabledDays);
+
+    @Query("SELECT enableddays FROM ticketentryfullday LIMIT 1")
+    Flux<String> obtenerEnabledDays();
 
 }
