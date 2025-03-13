56c57b95b6b27eb15bb18cfc982ad8e87eb823ed
ErickLG281 <elozanogomez281@gmail.com>
2025-03-13 09:14:23 -0500

    Added services for the Fullday Payment Application

--- src/main/java/com/proriberaapp/ribera/services/client/FullDayService.java
+++ src/main/java/com/proriberaapp/ribera/services/client/FullDayService.java
@@ -1,5 +1,6 @@
 package com.proriberaapp.ribera.services.client;
 
+import com.proriberaapp.ribera.Domain.dto.PaymentDetailFulldayDTO;
 import com.proriberaapp.ribera.Domain.entities.FullDayDetailEntity;
 import com.proriberaapp.ribera.Domain.entities.FullDayEntity;
 import com.proriberaapp.ribera.Domain.entities.FullDayFoodEntity;
@@ -15,8 +16,12 @@ public interface FullDayService {
 
     Mono<Void> saveFood(List<FullDayDetailEntity> savedDetails, List<FullDayFoodEntity> foods);
 
-    Flux<FullDayEntity> getReservationsByAssociatedId(Integer id, String filterType);
+    Flux<FullDayEntity> getReservationsByAssociatedId(Integer id, String filterType, Integer bookingStateId);
 
     Mono<FullDayEntity> findById(Integer id);
+
+    Flux<FullDayEntity> getReservationsAll();
+
+    Flux<PaymentDetailFulldayDTO> getPaymentDetailFullday();
 
 }
