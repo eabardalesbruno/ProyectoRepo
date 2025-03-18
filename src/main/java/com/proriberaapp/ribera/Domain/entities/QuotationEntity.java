d0f281796925e2f452d4b7f7d091ddb4af8246a1
Maycol Yauri <yauri_95@hotmail.com>
2025-03-18 17:51:08 -0500

    save with rewards

--- src/main/java/com/proriberaapp/ribera/Domain/entities/QuotationEntity.java
+++ src/main/java/com/proriberaapp/ribera/Domain/entities/QuotationEntity.java
@@ -11,6 +11,7 @@ import org.springframework.data.relational.core.mapping.Table;
 import com.proriberaapp.ribera.Api.controllers.client.dto.quotationDayDto;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
 
 @Getter
@@ -39,5 +40,17 @@ public class QuotationEntity {
 
     @Column("adult_extra_cost")
     private BigDecimal adultExtraCost;
+
+    @Column("kid_reward")
+    private BigInteger kidReward;
+
+    @Column("adult_reward")
+    private BigInteger adultReward;
+
+    @Column("adult_mayor_reward")
+    private BigInteger adultMayorReward;
+
+    @Column("adult_extra_reward")
+    private BigInteger adultExtraReward;
 
 }
