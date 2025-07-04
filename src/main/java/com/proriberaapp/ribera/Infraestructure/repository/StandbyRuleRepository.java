f1fcb0705b61720ad538a00a6972553dec33ec22
BryanPool99 <bryanpool991@hotmail.com>
2025-07-04 16:08:00 -0500

    feat: agregado de servicios para funcionalidad de modulo  regla de espera

---
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/repository/StandbyRuleRepository.java
@@ -0,0 +1,57 @@
+package com.proriberaapp.ribera.Infraestructure.repository;
+
+import com.proriberaapp.ribera.Api.controllers.admin.dto.occupancyConfiguration.standbyRules.response.StandByRuleDetailDto;
+import com.proriberaapp.ribera.Api.controllers.admin.dto.occupancyConfiguration.standbyRules.response.StandByRuleDto;
+import com.proriberaapp.ribera.Domain.entities.StandbyRuleEntity;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface StandbyRuleRepository extends R2dbcRepository<StandbyRuleEntity, Integer> {
+
+    @Query(value = """
+            SELECT
+                COUNT(sr.id_standby_rule) AS total_filtered_records
+            FROM standby_rule sr
+            INNER JOIN reservation_time_type rtt
+                ON rtt.id_reservation_time_type = sr.id_reservation_time_type
+            WHERE
+                (:searchTerm IS NULL)
+                OR (
+                    TRIM(UPPER(rtt.type_name)) LIKE '%' || TRIM(UPPER(:searchTerm)) || '%'
+                );
+            """)
+    Mono<Integer> countListStandByRules(String searchTerm);
+
+    @Query(value = """
+            SELECT
+                sr.id_standby_rule AS idstandbyrule,
+                rtt.type_name AS reservationtimetypename,
+                rtt.standby_hours AS standbyhours
+            FROM standby_rule sr
+            INNER JOIN reservation_time_type rtt
+                ON rtt.id_reservation_time_type = sr.id_reservation_time_type
+            WHERE
+                (:searchTerm IS NULL)
+                OR (
+                    TRIM(UPPER(rtt.type_name)) LIKE '%' || TRIM(UPPER(:searchTerm)) || '%'
+                )
+            LIMIT :size
+            OFFSET :offset;
+            """)
+    Flux<StandByRuleDto> getListStandByRules(String searchTerm, Integer size, Integer offset);
+
+    @Query(value = """
+            SELECT
+                sr.id_standby_rule AS idstandbyrule,
+                sr.id_reservation_time_type AS idreservationtimetype,
+                rtt.standby_hours AS standbyhours,
+                sr.id_visibility_type AS idvisibilitytype
+            FROM standby_rule sr
+            INNER JOIN reservation_time_type rtt
+                ON rtt.id_reservation_time_type = sr.id_reservation_time_type
+            WHERE sr.id_standby_rule = :id;
+            """)
+    Mono<StandByRuleDetailDto> getStandByRuleDetail(Integer id);
+}
