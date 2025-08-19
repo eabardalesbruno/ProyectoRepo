23848f8dc0fe442a10f28774f5ea0a41614fc507
Miguel Medina <migangmedina@gmail.com>
2025-08-19 10:38:42 -0500

    Add: Navbar Notification Mapper

---
+++ src/main/java/com/proriberaapp/ribera/Domain/mapper/NotificationBookingMapper.java
@@ -0,0 +1,28 @@
+package com.proriberaapp.ribera.Domain.mapper;
+
+import com.proriberaapp.ribera.Domain.dto.NotificationDto;
+import com.proriberaapp.ribera.Domain.entities.NotificationBookingEntity;
+
+public class NotificationBookingMapper {
+    public static NotificationDto toDto(NotificationBookingEntity notificationEntity){
+        return NotificationDto.builder()
+                .createdAt(notificationEntity.getCreatedAt())
+                .notificationTitle(notificationEntity.getNotificationTitle())
+                .notificationMessage(notificationEntity.getNotificationMessage())
+                .notificationType(notificationEntity.getNotificationType())
+                .userClientId(notificationEntity.getUserClientId())
+                .notificationIsRead(notificationEntity.isNotificationIsRead())
+                .build();
+    }
+
+    public static NotificationBookingEntity toEntity(NotificationDto notificationDto){
+        return NotificationBookingEntity.builder()
+                .createdAt(notificationDto.getCreatedAt())
+                .notificationTitle(notificationDto.getNotificationTitle())
+                .notificationMessage(notificationDto.getNotificationMessage())
+                .notificationType(notificationDto.getNotificationType())
+                .userClientId(notificationDto.getUserClientId())
+                .notificationIsRead(notificationDto.isNotificationIsRead())
+                .build();
+    }
+}
