1c6951c3c56dda4369e85fad84f2c32a0bcc70a5
Miguel Medina <migangmedina@gmail.com>
2025-08-22 16:14:26 -0500

    Add: PaymentVoucher entity and repository

---
+++ src/main/java/com/proriberaapp/ribera/Domain/entities/PaymentVoucherEntity.java
@@ -0,0 +1,42 @@
+package com.proriberaapp.ribera.Domain.entities;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Builder
+@Table("paymentvoucher")
+public class PaymentVoucherEntity {
+    @Id
+    @Column("voucherid")
+    private Integer voucherId;
+
+    @Column("paymentbookid")
+    private Integer paymentBookId;
+
+    @Column("currency")
+    private Integer currency;
+
+    @Column("operation")
+    private Integer operation;
+
+    @Column("codeoperation")
+    private String codeOperation;
+
+    @Column("note")
+    private String note;
+
+    @Column("totalcost")
+    private BigDecimal totalCost;
+
+    @Column("filename")
+    private String fileName;
+}
