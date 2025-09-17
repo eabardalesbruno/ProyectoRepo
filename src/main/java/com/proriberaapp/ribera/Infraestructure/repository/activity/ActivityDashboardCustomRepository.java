e20939e3e65f10f27c7f39e63cccab175510a32a
Ana <anayfernandez95@gmail.com>
2025-09-17 18:37:59 -0500

    feat: added ActivityProjection class

--- src/main/java/com/proriberaapp/ribera/Infraestructure/repository/activity/ActivityDashboardCustomRepository.java
+++ src/main/java/com/proriberaapp/ribera/Infraestructure/repository/activity/ActivityDashboardCustomRepository.java
@@ -2,13 +2,14 @@ package com.proriberaapp.ribera.Infraestructure.repository.activity;
 
 import java.time.LocalDateTime;
 
+import com.proriberaapp.ribera.Domain.Interfaces.ActivityRoomProjection;
 import com.proriberaapp.ribera.Domain.dto.activity.RoomDetailDTO;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ActivityDashboardCustomRepository {
-    Flux<RoomDetailDTO> findAllRoomsPaginated(LocalDateTime dateStart, LocalDateTime dateEnd, int size,
+    Flux<ActivityRoomProjection> findAllRoomsPaginated(LocalDateTime dateStart, LocalDateTime dateEnd, int size,
             int offset);
 
     Mono<Long> countAllRoomsFiltered(LocalDateTime dateStart, LocalDateTime dateEnd);
